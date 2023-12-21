98a4938af576799fd3d32751abe069bcd071b18d
devopspractice555 <146066476+devopspractice555@users.noreply.github.com>
2023-12-21 08:45:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz-"
+    bucket         = "abhishek-s3-demo-xyz-pt-asd"
     key            = "abhi1/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
