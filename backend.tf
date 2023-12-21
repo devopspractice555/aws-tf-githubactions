beb5908c604fd0f6a1e385be7d4b2803f0788295
devopspractice555 <devopspractice555@gmail.com>
2023-12-21 11:52:57 +0530

    added new file

--- backend.tf
+++ backend.tf
@@ -16,7 +16,7 @@ terraform {
 }
 provider "aws" {
   region = "ap-south-1"
-  # access_key = "${ secrets.AWS_ACCESS_KEY_ID }"
-  # secret_key = "${ secrets.AWS_SECRET_ACCESS_KEY }"
+  access_key = "AKIA3CVNHILEMHR3ITFQ"
+  secret_key = "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"
 
 }
