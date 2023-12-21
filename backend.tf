939390cdb0a4845cacad63a7035de8ccfa656fa3
devopspractice555 <devopspractice555@gmail.com>
2023-12-21 13:14:27 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,19 +1,27 @@
 
-terraform {
-  cloud {
-    organization = "tfpractice1"
+# terraform {
+#   cloud {
+#     organization = "tfpractice1"
 
-    workspaces {
-      name = "githubcicd"
-    }
+#     workspaces {
+#       name = "githubcicd"
+#     }
     
-  }
+#   }
  
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.29.0"
-    }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.29.0"
+#     }
+#   }
+# }
+
+terraform {
+  backend "s3" {
+    bucket = "abhishek-s3-demo-xyz-pt-asdhl"
+    key    = "github-actions-demo.tfstate"
+    region = "us-east-1"
   }
 }
 
--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz-pt-asdmmk"
+  bucket = "abhishek-s3-demo-xyz-pt-asdhl"
 }
