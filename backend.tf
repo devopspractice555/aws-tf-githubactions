3dc9281227c83f37000abbfe9de591a6d9356db7
devopspractice555 <devopspractice555@gmail.com>
2023-12-21 09:18:48 +0530

    Add test folder with files

--- backend.tf
+++ backend.tf
@@ -1,9 +1,12 @@
 terraform {
-  backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz-pt-asd"
-    key            = "abhi1/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+  cloud {
+    organization = "tfpractice1"
+
+    workspaces {
+      name = "githubcicd"
+    }
     
   }
+  
+  
 }
